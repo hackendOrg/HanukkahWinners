f75025fde67140affb38bfe90ba2b113a34d4f27
Michael <michael.e@outlook.co.il>
2018-12-05 21:30:35 +0200

    its workig

--- score_board.py
+++ score_board.py
@@ -9,7 +9,6 @@ class ScoreBoard:
         print_to_html(self.score_board, "results.html")
         self.driver = WebDriver()
 
-
     def add(self, name, score):
         self.score_board[name] = score
         print_to_html(self.score_board, "results.html")
